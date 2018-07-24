8419a4099155d21253aec60a3ebce79e599db080
王艳兵 <664956140@qq.com>
2018-07-24 16:42:25 +0800

    ByteBuf方法例子

---
+++ fanyin-web/src/test/java/com/fanyin/test/io/netty/ByteBufMain.java
@@ -0,0 +1,43 @@
+package com.fanyin.test.io.netty;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+
+/**
+ * @author 王艳兵
+ * @date 2018/7/21 13:54
+ */
+public class ByteBufMain {
+
+    public static void main(String[] args) throws Exception{
+        ByteBuf byteBuf = Unpooled.buffer(100);
+        byteBuf.writeBytes("fanyin".getBytes());
+        ByteBuf buf = Unpooled.buffer(100);
+        buf.writeBytes("erge".getBytes());
+        byteBuf.readBytes(buf,0,6);
+        System.out.println(buf);
+        int i = buf.readableBytes();
+        byte[] bytes = new byte[i];
+        buf.readBytes(bytes);
+        System.out.println(new String(bytes,"UTF-8"));
+
+        ByteBuf slice = Unpooled.buffer(100);
+        slice.writeBytes("abcdefghijklmnoprstuvwxyz".getBytes());
+        byte[] read = new byte[6];
+        slice.readBytes(read);
+        System.out.println("读取的:"+ new String(read,"utf-8"));
+        ByteBuf slice1 = slice.slice();
+        byte[] read2 = new byte[slice1.readableBytes()];
+        slice1.readBytes(read2);
+        System.out.println("slice:" + new String(read2,"utf-8"));
+
+        ByteBuf writerBuf = Unpooled.buffer(100);
+        writerBuf.writeBytes("abcdef".getBytes());
+        ByteBuf readBuf = Unpooled.buffer(100);
+        readBuf.writeBytes("xxggg".getBytes());
+        writerBuf.writeBytes(readBuf,2,3);
+        byte[] strByte = new byte[writerBuf.readableBytes()];
+        writerBuf.readBytes(strByte);
+        System.out.println("read:" + new String (strByte,"UTF-8"));
+    }
+}
---
+++ fanyin-web/src/test/java/com/fanyin/test/io/nio/ByteBufferMain.java
@@ -0,0 +1,27 @@
+package com.fanyin.test.io.nio;
+
+import java.nio.ByteBuffer;
+
+/**
+ * ByteBuffer说明
+ * @author 王艳兵
+ * @date 2018/7/20 17:29
+ */
+public class ByteBufferMain {
+
+    public static void main(String[] args) throws Exception{
+        ByteBuffer buffer = ByteBuffer.allocate(100);
+        String str = "fanyin";
+        buffer.put(str.getBytes());
+        //将总容量设置为当前位置,将当前位置设置0
+        buffer.flip();
+        //实际占用的长度
+        byte[] bytes = new byte[buffer.remaining()];
+        buffer.get(bytes);
+        System.out.println(new String(bytes,"UTF-8"));
+        //相当于重新读取 position=0 limit=6 capacity=100
+        buffer.rewind();
+        buffer.get(bytes);
+        System.out.println(new String(bytes,"UTF-8"));
+    }
+}
