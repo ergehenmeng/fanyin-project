96c2c0ac1f19849ecc8cf7aa0cb80a31c519cb16
王艳兵 <664956140@qq.com>
2018-10-08 16:24:01 +0800

    https://leetcode.com/problems/valid-parentheses/

---
+++ fanyin-web-manage/src/test/java/com/fanyin/test/leetcode/Valid.java
@@ -0,0 +1,45 @@
+package com.fanyin.test.leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
+
+/**
+ * @author 王艳兵
+ * @date 2018/10/8 15:48
+ */
+public class Valid {
+
+    public static void main(String[] args) {
+        System.out.println(isValid("]"));;
+    }
+
+    private static final Map<Character,Character> MAP = new HashMap<>();
+
+    static {
+        MAP.put(')','(');
+        MAP.put(']','[');
+        MAP.put('}','{');
+    }
+
+    private static boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0,length = s.length(); i < length; i++ ){
+            char c = s.charAt(i);
+            Character character = MAP.get(c);
+            if(character != null){
+                if(stack.isEmpty()){
+                    return false;
+                }
+                Character pop = stack.pop();
+                if(!pop.equals(character)){
+                    return false;
+                }
+            }else{
+                stack.push(c);
+            }
+        }
+        return stack.isEmpty();
+    }
+}
