27ed8431fbd1b9bbd93690b9819f2cc80d4394ba
二哥很猛 <664956140@qq.com>
2019-01-24 13:43:39 +0800

    valid校验完善

--- fanyin-common/src/main/java/com/fanyin/validation/annotation/SectionString.java
+++ fanyin-common/src/main/java/com/fanyin/validation/annotation/SectionString.java
@@ -1,12 +1,27 @@
 package com.fanyin.validation.annotation;
 
+import com.fanyin.validation.SectionStringDefine;
+
+import javax.validation.Constraint;
 import javax.validation.Payload;
+import javax.validation.ReportAsSingleViolation;
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.*;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 /**
  * 字符串限制
  * @author 二哥很猛
  * @date 2018/11/9 10:35
  */
+@Documented
+@Constraint(validatedBy = SectionStringDefine.class)
+@Target({ METHOD, FIELD, ANNOTATION_TYPE, CONSTRUCTOR, PARAMETER })
+@Retention(RUNTIME)
+@ReportAsSingleViolation
 public @interface SectionString {
 
     /**
