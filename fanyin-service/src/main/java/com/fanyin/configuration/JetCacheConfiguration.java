d6f8d0f133c14c8b7a4ae925b1b7c2f997b39959
二哥很猛 <664956140@qq.com>
2019-01-25 14:46:43 +0800

    优化maven依赖,启用lettuce客户端链接redis

--- fanyin-service/src/main/java/com/fanyin/configuration/JetCacheConfiguration.java
+++ fanyin-service/src/main/java/com/fanyin/configuration/JetCacheConfiguration.java
@@ -1,28 +1,8 @@
 package com.fanyin.configuration;
 
-import com.alicp.jetcache.CacheBuilder;
-import com.alicp.jetcache.anno.CacheConsts;
-import com.alicp.jetcache.anno.config.EnableCreateCacheAnnotation;
-import com.alicp.jetcache.anno.config.EnableMethodCache;
-import com.alicp.jetcache.anno.support.GlobalCacheConfig;
 import com.alicp.jetcache.anno.support.SpringConfigProvider;
-import com.alicp.jetcache.embedded.EmbeddedCacheBuilder;
-import com.alicp.jetcache.embedded.LinkedHashMapCacheBuilder;
-import com.alicp.jetcache.redis.RedisCacheBuilder;
-import com.alicp.jetcache.support.FastjsonKeyConvertor;
-import com.alicp.jetcache.support.JavaValueDecoder;
-import com.alicp.jetcache.support.JavaValueEncoder;
-import com.google.common.collect.Maps;
-import org.apache.commons.pool2.impl.GenericObjectPoolConfig;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-import redis.clients.jedis.Jedis;
-import redis.clients.jedis.JedisPool;
-import redis.clients.util.Pool;
-
-import java.util.Map;
 
 /**
  * jetCache缓存配置
@@ -63,44 +43,12 @@ public class JetCacheConfiguration {
     @Value("${max-total:10}")
     private int maxTotal;
 
-    @Bean
-    public Pool<Jedis> pool(){
-        GenericObjectPoolConfig config = new GenericObjectPoolConfig();
-        config.setMaxIdle(maxIdle);
-        config.setMaxTotal(maxTotal);
-        config.setMinIdle(minIdle);
-        return new JedisPool(config, host, port,timeout);
-    }
 
     @Bean
     public SpringConfigProvider provider(){
         return new SpringConfigProvider();
     }
 
-    @Bean
-    public GlobalCacheConfig config(SpringConfigProvider configProvider, Pool<Jedis> pool){
 
-        Map<String,CacheBuilder> localBuilder = Maps.newHashMap();
-        EmbeddedCacheBuilder localCacheBuilder = LinkedHashMapCacheBuilder.createLinkedHashMapCacheBuilder()
-                .keyConvertor(FastjsonKeyConvertor.INSTANCE);
-        localBuilder.put(CacheConsts.DEFAULT_AREA,localCacheBuilder);
-
-        Map<String,CacheBuilder> remoteBuilder = Maps.newHashMap();
-        RedisCacheBuilder remoteCacheBuilder = RedisCacheBuilder.createRedisCacheBuilder()
-                .keyConvertor(FastjsonKeyConvertor.INSTANCE)
-                .valueDecoder(JavaValueDecoder.INSTANCE)
-                .valueEncoder(JavaValueEncoder.INSTANCE)
-                .jedisPool(pool);
-
-        remoteBuilder.put(CacheConsts.DEFAULT_AREA,remoteCacheBuilder);
-
-        GlobalCacheConfig config = new GlobalCacheConfig();
-        config.setConfigProvider(configProvider);
-        config.setRemoteCacheBuilders(remoteBuilder);
-        config.setLocalCacheBuilders(localBuilder);
-        config.setStatIntervalMinutes(15);
-        config.setAreaInCacheName(false);
-        return config;
-    }
 
 }
